861967915cdf550242786c80bc70e4a1ea23fd0a
acortes417 <acortes417@gmail.com>
2018-03-08 17:47:10 -0800

    Update nba_main.py

--- nba_main.py
+++ nba_main.py
@@ -1,5 +1,53 @@
 from pathlib import Path
+nba_2017_season = "https://www.basketball-reference.com/leagues/NBA_2017.html"
+teams = {'boston celtics': 'BOS', 'cleveland cavaliers' : 'CLE', 'toronto raptors' : 'TOR',
+        'washington wizards':'WAS', 'atlanta hawks':'ATL', 'milwaukee bucks':'MIL',
+        'indiana pacers':'IND','chicago bulls':'CHI','miami heat':'MIA','detroit pistons':'DET',
+        'charlotte hornets':'CHA','new york knicks': 'NYK','orlando magic': 'ORL', 'philadelphia 76ers':'PHI',
+        'brooklyn nets':'BKN','golden state warriors':"GSW",'san antonio spurs':'SAS','houston rockets':'HOU', 
+        'los angeles clippers':'LAC', 'utah jazz': 'UTA', 'oklahoma city thunder':'OKC','memphis grizzlies':'MEM', 
+        'portland trail blazers':'POR', 'denver nuggets':'DEN', 'new orleans pelicans':'NOP', 'dallas mavericks':'DAL', 
+        'sacramento kings':'SAC', 'minnesota timberwolves':'MIn', 'los angeles lakers':'LAL','phoenix suns':'PHX'}
 
+#builds a url so we can look for that specific team
+def build_team_url(team):
+    values = teams.keys()
+    team = team.lower()
+    try:
+        for key in values:
+            if key.find(team) >= 0:#if you put maimi or just clippers it will give you the correct abreviation  
+                team = teams[key]
+                return "https://www.basketball-reference.com/teams/" + team + "/2017.html"
+        print("Sorry couldn't Find your team")
+        return
+    except KeyError:
+        print("sorry No Team by That Name")
+ #scrapes the data of a specific player
+def scrape_player(url):
+    html = urllib.urlopen(url)
+    
+    soup = BeautifulSoup(html, 'lxml')
+    
+    table = soup.find('table')
+    
+    header = []
+    table_headers = table.find_all('thead')#we get the headers of each collumn
+    for th in table_headers:
+        t = th.find_all('th')
+        header = [i.text for i in t]
+    print(header)
+    
+    other = []
+    advanced_List = []
+    table_rows = table.find_all('tr')
+    for tr in table_rows:
+        a = tr.find_all('a')
+        td = tr.find_all('td')
+        advanced_List = [i.text for i in a]#makes a list containing dates
+        if advanced_List and advanced_List[0] == '2016-17':#checks date 
+            other += [i.text for i in td]#inculdes it to our other list
+    print(other)
+    
 def nba_options():
     print("1. Conference")
     print("2. Team")
