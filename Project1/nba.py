4a6ebd66e295af929ff92d9577c39158510dddef
Tristan Manning <32554651+pikachugottapoo@users.noreply.github.com>
2018-03-12 01:04:44 -0700

    Update and rename nbaFunctions.py to nba.py

--- Project1/nba.py
+++ Project1/nba.py
@@ -27,11 +27,11 @@ class Conference:
             writer.writerow(self.get_teams()[0])
             writer.writerow(self.get_teams()[1:])
 
-    def __str__(self):
-        teams = self._teams[0]._name
-        for team in self._teams[1:]:
-            teams += ', ' + team._name
-        return f"Name: {self._name}\nTeams: {teams}"
+    def display(self):
+        print(self.get_name() + '\n')
+        for line in self.get_teams():
+            print(line)
+
 
 # team class has two data attributes.
 # name is string type
@@ -62,17 +62,24 @@ class Team:
     def salary_cap(self):
         pass
 
-    def __str__(self):
-        players = self._players[0]._name
-        for player in self._players[1:]:
-            players += ', ' + player._name
-        return f"Name: {self._name}\nPlayers: {players}"
+    def display(self):
+        team = self.get_players()
+        string = ''
+        team[0].pop(6)
+        for i in team[0]:
+            string += '{message:{fill}{align}{width}}'.format(message=i, fill=' ', align='<', width=23)
+        string += '\n'
+        for i in team[1:]:
+            i.pop(6)
+        for j, c in enumerate(i):
+            string += '{message:{fill}{align}{width}}'.format(message=c, fill='', align='<', width=23)
+        string += '\n'
+        print(string)
 
-# player class has four data attributes.
-# name is string type
-# ppg is float type
-# apg is float type
-# rpg is float type
+
+# player class has one data attribute.
+# player is a list of a player's stats
+
 class Player:
     def __init__(self, player):
         self._player = player
@@ -110,35 +117,41 @@ class Player:
             writer.writerow(self.get_player()[1:])
 
     def display(self):
-        pass
+        team = self.get_player()
+        string = ''
+        for i in team[0]:
+            string += '{message:{fill}{align}{width}}'.format(message=i, fill=' ', align='<', width=8)
+        string += '\n'
+        for i in team[1:]:
+            string += '{message:{fill}{align}{width}}'.format(message=i, fill='', align='<', width=8)
+        string += '\n'
+        print(string)
 
-    def __str__(self):
-        #return f"Name: {self._name}\nPPG: {self._ppg}\nAPG: {self._apg}\nRPG: {self._rpg}"
-        pass
 
 
-def main():
-    # kobe = Player("Kobe Bryant", 31.2, 5.2, 7.1)
-    # derek = Player("Derek Fisher", 19.7, 7.2, 4.6)
-    # chris = Player("Chris Paul", 25.5, 6.7, 4.2)
-    # blake = Player("Blake Griffin", 15.6, 2.4, 10.1)
-    # laker_players = [kobe, derek]
-    # clipper_players = [chris, blake]
-    # lakers = Team("Lakers", laker_players)
-    # clippers = Team("Clippers", clipper_players)
-    # west_teams = [lakers, clippers]
-    # west_conf = Conference("Western Conference", west_teams)
-    # print()
-    # print(kobe)
-    # print()
-    # print(lakers)
-    # print()
-    # print(west_conf)
-    header = ["a", "b"]
-    c = ['Cole Aldrich', 'C', '28', 'MIN', '62', '0', '531', '45', '86', '.523', '0', '0', '',
-    '45', '86', '.523', '.523', '15', '22', '.682', '51', '107', '158', '25', '25', '23', '17', '85', '105']
-    g = Player(c)
-    g.create_player_csv()
+# def main():
+#     kobe = Player("Kobe Bryant", 31.2, 5.2, 7.1)
+#     derek = Player("Derek Fisher", 19.7, 7.2, 4.6)
+#     chris = Player("Chris Paul", 25.5, 6.7, 4.2)
+#     blake = Player("Blake Griffin", 15.6, 2.4, 10.1)
+#     laker_players = [kobe, derek]
+#     clipper_players = [chris, blake]
+#     lakers = Team("Lakers", laker_players)
+#     clippers = Team("Clippers", clipper_players)
+#     west_teams = [lakers, clippers]
+#     west_conf = Conference("Western Conference", west_teams)
+#     print()
+#     print(kobe)
+#     print()
+#     print(lakers)
+#     print()
+#     print(west_conf)
+#     header = ["a", "b"]
+#     c = [header, 'Cole Aldrich', 'C', '28', 'MIN', '62', '0', '531', '45', '86', '.523', '0', '0', '0',
+#     '45', '86', '.523', '.523', '15', '22', '.682', '51', '107', '158', '25', '25', '23', '17', '85', '105']
+#     g = Player(c)
+#     g.create_player_csv()
+#     g.display()
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
