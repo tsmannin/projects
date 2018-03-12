6df73c9d99bb233c8d8b9605cb058ed47c79e699
acortes417 <acortes417@gmail.com>
2018-03-12 16:21:38 -0700

    Update nba.py

--- Project1/nba.py
+++ Project1/nba.py
@@ -1,12 +1,3 @@
-#hey guys, heres the class structures for our project. maybe after Andre creates the functions to scrape the data,
-#Hieu can create the csv files so that we can pass it into our main function to create our class objects easier?
-#feel free to add more functions Tristan!
-
-
-
-# conference class has two data attributes.
-# name is string type
-# teams is list type containing team objects
 import csv
 
 class Conference:
@@ -24,16 +15,20 @@ class Conference:
         filename = self.get_name() + ".csv"
         with open(filename, 'w', newline = "") as f:
             writer = csv.writer(f)
+            self._teams[0].pop(0)
+            self._teams[0].insert(0,"Team")
             writer.writerow(self.get_teams()[0])
-            writer.writerow(self.get_teams()[1:])
+            for i in self._teams[1:]:
+                writer.writerow(i)
 
     def display(self):
-        team = selg.get_teams()
+        team = self.get_teams()
         string = ''
         for i in team[0]:
             string += '{message:{fill}{align}{width}}'.format(message=i, fill=' ', align='<', width=25)
         string += '\n'
         team[1].pop(4)
+        team[1].insert(4,'--')
         for i in team[1:]:
             for j in i:
                 string += '{message:{fill}{align}{width}}'.format(message=j, fill='', align='<', width=25)
@@ -60,11 +55,14 @@ class Team:
 
     def create_team_csv(self):
         # not sure if headers is at 0 or name is
-        filename = self.get_players()[1] + ".csv"
+        #print(self.get_players()[1], type(self.get_players()[1]))
+        filename = self._name.upper() + ".csv"
         with open(filename, 'w', newline = "") as f:
             writer = csv.writer(f)
             writer.writerow(self.get_players()[0])
-            writer.writerow(self.get_players()[1:])
+            for i in self.get_players()[1:]:
+                writer.writerow(i)
+                #writer.writerow()
 
     #salary cap is adding all the players salary on the team for that year
     def salary_cap(self):
@@ -79,9 +77,9 @@ class Team:
         string += '\n'
         for i in team[1:]:
             i.pop(6)
-        for j, c in enumerate(i):
-            string += '{message:{fill}{align}{width}}'.format(message=c, fill='', align='<', width=23)
-        string += '\n'
+            for j, c in enumerate(i):
+                string += '{message:{fill}{align}{width}}'.format(message=c, fill='', align='<', width=23)
+            string += '\n'
         print(string)
 
 
@@ -89,9 +87,9 @@ class Team:
 # player is a list of a player's stats
 
 class Player:
-    def __init__(self, player):
+    def __init__(self, name ,player):
         self._player = player
-
+        self._name = name
     def get_player(self):
         return self._player
 
@@ -118,7 +116,7 @@ class Player:
 
     def create_player_csv(self):
         # not sure if headers is at 0 or name is
-        filename = self.get_player()[1] + ".csv"
+        filename = self._name + ".csv"
         with open(filename, 'w', newline = "") as f:
             writer = csv.writer(f)
             writer.writerow(self.get_player()[0])
@@ -134,32 +132,3 @@ class Player:
             string += '{message:{fill}{align}{width}}'.format(message=i, fill='', align='<', width=8)
         string += '\n'
         print(string)
-
-
-
-# def main():
-#     kobe = Player("Kobe Bryant", 31.2, 5.2, 7.1)
-#     derek = Player("Derek Fisher", 19.7, 7.2, 4.6)
-#     chris = Player("Chris Paul", 25.5, 6.7, 4.2)
-#     blake = Player("Blake Griffin", 15.6, 2.4, 10.1)
-#     laker_players = [kobe, derek]
-#     clipper_players = [chris, blake]
-#     lakers = Team("Lakers", laker_players)
-#     clippers = Team("Clippers", clipper_players)
-#     west_teams = [lakers, clippers]
-#     west_conf = Conference("Western Conference", west_teams)
-#     print()
-#     print(kobe)
-#     print()
-#     print(lakers)
-#     print()
-#     print(west_conf)
-#     header = ["a", "b"]
-#     c = [header, 'Cole Aldrich', 'C', '28', 'MIN', '62', '0', '531', '45', '86', '.523', '0', '0', '0',
-#     '45', '86', '.523', '.523', '15', '22', '.682', '51', '107', '158', '25', '25', '23', '17', '85', '105']
-#     g = Player(c)
-#     g.create_player_csv()
-#     g.display()
-
-# if __name__ == "__main__":
-#     main()
