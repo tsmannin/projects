5e29bbfb8fe79056f59fda7db23ae2f8af1d6b3f
acortes417 <acortes417@gmail.com>
2018-03-12 15:46:16 -0700

    Update scrape.py

--- Project1/scrape.py
+++ Project1/scrape.py
@@ -1,10 +1,9 @@
 from bs4 import BeautifulSoup
 import urllib
-import pandas as pd
 import re
-
 def scrape_conference(conference):
-    html = urllib.urlopen(url)
+    url = 'https://www.basketball-reference.com/leagues/NBA_2017.html'
+    html = urllib.request.urlopen(url)
 
     soup = BeautifulSoup(html, 'lxml')
 
@@ -16,12 +15,14 @@ def scrape_conference(conference):
     table = soup.find_all('tbody')
     for i,c in enumerate(tabh):
         conf = c.text.lower()
-
-        if conf.find(conference) > 0 and i < 2:#not case sensitive
+        if conf.find(conference) > 0 and i <= 2:#not case sensitive
             td = c.find_all('th')
-            header.append([f.text for f in td])
+            for f in td:
+                header.append(f.text)
             row = i
-    if row > 0:   #if we find the correct conference it will not be -1
+        break
+            
+    if row >= 0:   #if we find the correct conference it will not be -1
         for i,c in enumerate(table):
             if i == row:
                 tr = c.find_all('tr')
@@ -35,20 +36,24 @@ def scrape_conference(conference):
     else:
         print("Sorry no conference Matching that name")
         return []
+    stats.insert(0,header)
     return stats
-
+    
 def scrape_team(url):
-    html = urllib.urlopen(url)
-    matchObj = re.match(r'https://www.basketball-reference.com/teams/(.*)/2017.html', url, re.M|re.I)
-
-    if matchObj:
+    matchObj = False
+    try: 
+        matchObj = re.match(r'https://www.basketball-reference.com/teams/(.*)/2017.html', url, re.M|re.I)
+    except TypeError:
+        print("need string")
+    if matchObj: 
+        html = urllib.request.urlopen(url)
         soup = BeautifulSoup(html, 'lxml')
         header = []
         tabh = soup.find_all('thead')
         for tr in tabh:
             tr = tr.find_all('th')
             header += [i.text for i in tr]#looks for headers
-
+        header.pop(6)
         tbody = soup.find_all('tbody')
         numbers = []
         roster = []
@@ -61,71 +66,39 @@ def scrape_team(url):
                 td = c.find_all('td')
                 roster.append([f.text for f in td])
                 roster[i].insert(0,numbers[i][0])#inludes number of each player
-
+                roster[i].pop(6)
         roster.insert(0,header)#includes headers to top of list
         return(roster)#returns a list of all players and headers
     else:
+        return []  
+      
+def scrape_player(url,year):
+    matchObj = re.match(r'https://www.basketball-reference.com/players/(.*)/(.*).html', url, re.M|re.I)
+    if matchObj:
+        html = urllib.request.urlopen(url)
+
+        soup = BeautifulSoup(html, 'lxml')
+
+        table = soup.find('table')
+        name = soup.find()
+        header = []
+        table_headers = table.find_all('thead')#we get the headers of each collumn
+        for th in table_headers:
+            t = th.find_all('th')
+            header = [i.text for i in t]
+        
+
+        other = []
+        advanced_List = []
+        table_rows = table.find_all('tr')
+        for tr in table_rows:
+            a = tr.find_all('a')
+            td = tr.find_all('td')
+            advanced_List = [i.text for i in a]#makes a list containing dates
+            if advanced_List and advanced_List[0] == year:#checks date
+                other.insert(0, advanced_List[0])
+                other += [i.text for i in td]#inculdes it to our other list
+        other.insert(0,header)
+        return other
+    else:
         return []
-
-
-
-def scrape_player():
-    url = "https://www.basketball-reference.com/leagues/NBA_2017_totals.html    "
-    html = urllib.request.urlopen(url)
-
-    soup = BeautifulSoup(html, 'lxml')
-
-    table = soup.find('table')
-
-    basic_stats = []
-    table_rows = table.find_all('tr')
-    for tr in table_rows:
-        td = tr.find_all('td')#looks through all <table data> tags to find data
-        basic_stats.append([i.text for i in td])#gets text from table data
-    return basic_stats
-    #returns a list of the basic stats of the players
-
-def scrape_advanced_stats():
-    url = "https://www.basketball-reference.com/leagues/NBA_2017_advanced.html"
-    html = urllib.request.urlopen(url)
-
-    soup = BeautifulSoup(html, 'lxml')
-
-    table = soup.find('table')
-
-    advanced_List = []
-    table_rows = table.find_all('tr')
-    for tr in table_rows:
-        td = tr.find_all('td') #looks through all <table data> tags to find data
-        advanced_List.append([i.text for i in td])#conference list
-    return advanced_list
-
-def scrape_conference():
-    dfs = pd.read_html("https://www.basketball-reference.com/leagues/NBA_2017.html")
-
-    conference = []
-    for df in dfs:
-        conference += df.values.tolist()#iterates through data frame to turn each line into list
-
-    return conference
-
-if __name__ == "__main__":
-     main()
-#you also need to run this on python2(in terminal type alias python=python2; confirm with python --version to check)
-
-#i just need to find a way to find all the headers for every stat/conference or we can just organize them through
-#our functions in the classes
-
-# you need to install pandas to run this as well. Type
-- "pip install pandas"
-      or
-- sudo apt-get install python-pandas
-      or
-- sudo easy_install pandas
-
-to install beautifulsoup
--pip install bs4
-      or
--sudo apt-get install python-bs4
-      or
--sudo easy install bs4
