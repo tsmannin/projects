1f742023fdc506f8cc36aa0551c8e38bd97aff26
Tristan Manning <32554651+pikachugottapoo@users.noreply.github.com>
2018-03-12 19:17:55 -0700

    Update scrape.py

--- Project1/scrape.py
+++ Project1/scrape.py
@@ -232,4 +232,43 @@ def scrape_player(url, team):
     else:
         return []
 
+def scrape_conference(conference):
+    url = 'https://www.basketball-reference.com/leagues/NBA_2017.html'
+    html = urllib.request.urlopen(url)
+
+    soup = BeautifulSoup(html, 'lxml')
+
+    header = []
+    teams = []
+    stats = []
+    row = -1
+    tabh = soup.find_all('thead')
+    table = soup.find_all('tbody')
+    for i,c in enumerate(tabh):
+        conf = c.text.lower()
+        if conf.find(conference.lower()) > 0 and i <= 2:#not case sensitive
+            td = c.find_all('th')
+            for f in td:
+                header.append(f.text)
+            row = i
+        break
+
+    if row >= 0:   #if we find the correct conference it will not be -1
+        for i,c in enumerate(table):
+            if i == row:
+                tr = c.find_all('tr')
+                a = c.find_all('a')
+                for j in a:
+                    teams.append([j.text])
+                for f, c in enumerate(tr):
+                    td = c.find_all('td')
+                    stats.append([l.text for l in td])
+                    stats[f].insert(0,teams[f][0])
+    else:
+        print("Sorry no conference Matching that name")
+        return []
+    stats.insert(0,header)
+    return stats
+
+
 
